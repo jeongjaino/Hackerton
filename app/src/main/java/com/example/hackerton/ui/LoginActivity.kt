6b573190bbe321ba79e88e1c69227946192ac499
jaino <msb3302@naver.com>
2022-04-17 14:00:24 +0900

    inital commit

---
+++ app/src/main/java/com/example/hackerton/ui/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.hackerton.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
