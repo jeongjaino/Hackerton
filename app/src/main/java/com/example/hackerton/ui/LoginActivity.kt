0d15305b547dd46022d4ec069253f2a97d6a057d
jaino <msb3302@naver.com>
2022-04-17 16:16:27 +0900

    create signIn function

--- app/src/main/java/com/example/hackerton/ui/LoginActivity.kt
+++ app/src/main/java/com/example/hackerton/ui/LoginActivity.kt
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.hackerton.databinding.ActivityLoginBinding
+import com.example.hackerton.reqeust.SignInInfo
 import com.example.hackerton.reqeust.SignUpInfo
-import com.example.hackerton.response.SignUpResponse
 import com.example.hackerton.utils.RetrofitUtil
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.lang.Exception
 
 class LoginActivity : AppCompatActivity() {
@@ -18,9 +19,12 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(binding.root)
         binding.signUpButton.setOnClickListener {
             signUp()
+        }
+        binding.signInButton.setOnClickListener {
+            signIn()
         }
     }
 
@@ -29,7 +33,7 @@ class LoginActivity : AppCompatActivity() {
         val email = binding.emailEditText.text.toString()
         val password = binding.passwordEditText.text.toString()
 
-        if(email.isNotEmpty() && password.isNotEmpty()){
+        if(name.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()){
             CoroutineScope(Dispatchers.IO).launch{
                 try{
                     val response = RetrofitUtil.userService.userSignUp(
@@ -41,18 +45,64 @@ class LoginActivity : AppCompatActivity() {
                     )
                     if(response.isSuccessful){
                         val body = response.body()
-                        body?.let{ response ->
-                            Toast.makeText(this@LoginActivity, body.message, Toast.LENGTH_SHORT
-                            ).show()
+                        withContext(Dispatchers.Main) {
+                            body?.let { response ->
+                                Toast.makeText(this@LoginActivity,
+                                    response.message,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
                         }
                     } else{
-                        Toast.makeText(this@LoginActivity,"회원가입에 실패하였습니다.", Toast.LENGTH_SHORT
-                        ).show()
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(this@LoginActivity,"회원가입에 실패하였습니다.", Toast.LENGTH_SHORT
+                            ).show()
+                        }
                     }
                 } catch(e:Exception) {
                     e.printStackTrace()
                 }
             }
+        } else{
+            Toast.makeText(this@LoginActivity,"모든 정보를 입력하세요!.", Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+    private fun signIn(){
+        val email = binding.emailEditText.text.toString()
+        val password = binding.passwordEditText.text.toString()
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            CoroutineScope(Dispatchers.IO).launch{
+                try{
+                    val response = RetrofitUtil.userService.userSignIn(
+                        SignInInfo(
+                            email = email,
+                            password = password
+                        )
+                    )
+                    if(response.isSuccessful){
+                        val body = response.body()
+                        withContext(Dispatchers.Main) {
+                            body?.let { response ->
+                                Toast.makeText(this@LoginActivity,
+                                    response.token,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                    } else{
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(this@LoginActivity,"로그인에 실패하였습니다.", Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                } catch(e:Exception) {
+                    e.printStackTrace()
+                }
+            }
+        } else{
+            Toast.makeText(this@LoginActivity,"모든 정보를 입력하세요!.", Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
