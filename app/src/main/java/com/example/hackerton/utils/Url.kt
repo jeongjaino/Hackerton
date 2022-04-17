1a3a51df9a8844a6b0327cf337e3778149741382
jaino <msb3302@naver.com>
2022-04-17 14:20:35 +0900

    add util file - url

---
+++ app/src/main/java/com/example/hackerton/utils/Url.kt
@@ -0,0 +1,12 @@
+package com.example.hackerton.utils
+
+object Url {
+
+    const val URL = "http://58.229.6.137:3000/api-docs"
+
+    const val SIGN_IN = "/auth/signin"
+
+    const val SIGN_UP = "/auth/signUp"
+
+    const val GET_USER = "/user"
+}
